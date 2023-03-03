9e939800ec5cf31889f0457d30443066cd8769d9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-03-03 23:48:50 +0100

    Bonhomme se déplace

---
+++ Scripts/mouvementBonhomme.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class mouvementBonhomme : KinematicBody2D {
+    [Export] public int speed = 200;
+
+    public Vector2 velocity = new Vector2();
+
+    public void GetInput() {
+        velocity = new Vector2();
+
+        if (Input.IsActionPressed("move_right"))
+            velocity.x += 1;
+
+        if (Input.IsActionPressed("move_left"))
+            velocity.x -= 1;
+
+        if (Input.IsActionPressed("move_down"))
+            velocity.y += 1;
+
+        if (Input.IsActionPressed("move_up"))
+            velocity.y -= 1;
+
+        velocity = velocity.Normalized() * speed;
+    }
+
+    public override void _PhysicsProcess(float delta) {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }
+}
