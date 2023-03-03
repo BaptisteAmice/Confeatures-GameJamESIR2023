72e2627753ee9a7e71d7976d2fec6af11c95926b
hsfalt <tedjms17@gmail.com>
2023-03-04 00:33:35 +0100

    Bonhomme se déplace mais compil pas

--- Scripts/mouvementBonhomme.cs
+++ Scripts/mouvementBonhomme.cs
@@ -4,16 +4,19 @@ using System;
 public class mouvementBonhomme : KinematicBody2D {
     [Export] public int speed = 200;
 
-    public Vector2 velocity = new Vector2();
+    public Vector2 velocity=new Vector2();
+    public int sens;
 
     public void GetInput() {
         velocity = new Vector2();
 
         if (Input.IsActionPressed("move_right"))
             velocity.x += 1;
+            sens=0;
 
         if (Input.IsActionPressed("move_left"))
             velocity.x -= 1;
+            sens=1;
 
         if (Input.IsActionPressed("move_down"))
             velocity.y += 1;
@@ -26,6 +29,8 @@ public class mouvementBonhomme : KinematicBody2D {
 
     public override void _PhysicsProcess(float delta) {
         GetInput();
+        Sprite sprite = GetNode<Sprite>("BonhommeSprite");
+        sprite.SetFlipH(sens);
         velocity = MoveAndSlide(velocity);
     }
 }
