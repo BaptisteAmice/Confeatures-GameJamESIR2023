6b6c9d31c64c46bb3224c2766e16f6c5c179a1fa
hsfalt <llkinako0410@gmail.com>
2023-03-04 11:44:36 +0100

    Nouvelle animation + je sais plus quoi

--- Scripts/BonhommeSprite.cs
+++ Scripts/BonhommeSprite.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 
 public class BonhommeSprite : AnimatedSprite {
+    public int mort=0;
     public void SetAnimation() {
         if (Input.IsActionPressed("move_right") || Input.IsActionPressed("move_left")) {
             this.Playing=true;
@@ -18,7 +19,24 @@ public class BonhommeSprite : AnimatedSprite {
         }
     }
 
+    public void Death() {
+        this.FlipV=false;
+        //this.SelfModulate(1, 0.29, 0.29, 1);
+        mort=1;
+    }
+
+    public void Revivre() {
+        this.FlipV=true;
+        //base.SelfModulate(1, 1, 1, 1);
+        mort=0;
+    }
+
+
     public override void _PhysicsProcess(float delta) {
+        if (Input.IsActionPressed("suicide")) {Death();} 
+        else if (mort==1) {
+            Revivre();
+        }
         SetAnimation();
     }
 }
