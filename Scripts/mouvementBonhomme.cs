af45479f76fbb8b1fb4195a3dc45afe42276b781
hsfalt <llkinako0410@gmail.com>
2023-03-04 03:25:02 +0100

    BON FONCTIONNE LÀ

--- Scripts/mouvementBonhomme.cs
+++ Scripts/mouvementBonhomme.cs
@@ -5,30 +5,35 @@ public class mouvementBonhomme : KinematicBody2D {
     public Vector2 velocity=new Vector2();
 
     private const int speed=200;
-    private const int gravity=3000;
-    private const int jump=-10000;
+    private const int gravity=5000;
+    private const int jump=-9000;
 
     public void GetInput(float delta) {
-        velocity = new Vector2();
-        AnimatedSprite sprite = GetNode<AnimatedSprite>("BonhommeSprite");
+        velocity=new Vector2();
+        AnimatedSprite sprite=GetNode<AnimatedSprite>("BonhommeSprite");
+        Timer timer=GetNode<Timer>("TimerJump");
 
         if (Input.IsActionPressed("move_right")) {
-            velocity.x += speed;
+            velocity.x+=speed;
         }
 
         if (Input.IsActionPressed("move_left")) {
-            velocity.x -= speed;
+            velocity.x-=speed;
         }
 
-        if(IsOnFloor()) {
+        if(IsOnFloor() || (timer.TimeLeft>0)) {
+            if (!(timer.TimeLeft>0)) {
+                timer.Start((float)0.35);
+            }
             if(Input.IsActionPressed("move_up"))
-                velocity.y += jump;
+                velocity.y += delta*jump;
+        } else {
+            velocity.y+= delta*gravity;
         }
     }
 
     public override void _PhysicsProcess(float delta) {
         GetInput(delta);
-        velocity.y+= delta*gravity;
         velocity = MoveAndSlide(velocity, new Vector2(0,-1));
     }
 }
