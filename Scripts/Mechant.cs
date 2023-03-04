0c9af1e8b724068faf91c82a16e8d00a94223ae8
hsfalt <llkinako0410@gmail.com>
2023-03-04 19:30:48 +0100

    Changement2

--- Scripts/Mechant.cs
+++ Scripts/Mechant.cs
@@ -4,9 +4,23 @@ using System;
 public class Mechant : KinematicBody2D {
     private bool touchable=false;
 
+    void toucheMechant () {
+        touchable=true;
+    }
+
+    void pasToucheMechant () {
+        touchable=false;
+    }
+
     public override void _PhysicsProcess(float delta) {
-        if (Input.IsActionJustPressed("interect")) {
-			//TryBreak();
+        if (Input.IsActionJustPressed("interect") && touchable) {
+			AnimatedSprite sprite=GetNode<AnimatedSprite>("BonhommeSpriteMarche");
+            sprite.Rotation=0;
+            Vector2 position=this.Position;
+            position.x=0;
+            position.y=14;
+
+            sprite.Position=position;
 		}
     }
         
